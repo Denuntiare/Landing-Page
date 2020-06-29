aaf14a68c32900abe334fa746f6daae481fbfcda
Lucas <lucaspalmastolol@gmail.com>
2020-06-29 19:32:43 -0300

    Changes to Css

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// Toggle Navbar
-$(".BotaoTopo").click (function(){
-    $(this).toggleClass("open");
-    $("nav").toggleClass("open");
-});
-
 // Grup
 jQuery(document).ready(function() {
   
@@ -19,7 +13,5 @@ $(function() {
 	$('a').bind('click',function(event){
 	  var $anchor = $(this);
 	  $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1000,'swing');
-	  // Outras Animações
-	  // linear, swing, jswing, easeInQuad, easeInCubic, easeInQuart, easeInQuint, easeInSine, easeInExpo, easeInCirc, easeInElastic, easeInBack, easeInBounce, easeOutQuad, easeOutCubic, easeOutQuart, easeOutQuint, easeOutSine, easeOutExpo, easeOutCirc, easeOutElastic, easeOutBack, easeOutBounce, easeInOutQuad, easeInOutCubic, easeInOutQuart, easeInOutQuint, easeInOutSine, easeInOutExpo, easeInOutCirc, easeInOutElastic, easeInOutBack, easeInOutBounce
 	});
 });
