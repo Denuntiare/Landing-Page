9b359106031abcc0871e9f8899b77425822167ae
Lucas <lucaspalmastolol@gmail.com>
2020-07-01 10:40:40 -0300

    Added button back to Top
    Button for easy user navigation

--- main.js
+++ main.js
@@ -15,3 +15,20 @@ $(function() {
 	  $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1000,'swing');
 	});
 });
+
+// Botão Back to Top
+
+var btn = $('#back-to-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
